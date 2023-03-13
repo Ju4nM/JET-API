0f543ada9cb2d24d8d7922a1aac91812c32616b1
Ju4nM <jenriquemflores@gmail.com>
2023-03-13 14:21:04 -0700

    otra prueba

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.use(cookieParser());
-	app.useGlobalPipes(new ValidationPipe());
 	app.enableCors({
 		// origin: ["http://localhost:4200", "jetapptest.somee.com", "jetclimatizationapp.somee.com"],
 		// origin: "http://localhost:4200",
 		origin: "http://jetapptest.somee.com",
 		credentials: true
 	});
+	app.use(cookieParser());
+	app.useGlobalPipes(new ValidationPipe());
 	await app.listen(3000);
 }
 bootstrap();
